0cd07f36a13c78eead2fc0da70b7ec6570e1aac9
Matt Aitken <matt.aitken@gmail.com>
2017-09-17 17:36:42 +1000

    add 'placed' check
    check for robot placed status
    correction on original place function
    basic commands info added to html

--- simulator.js
+++ simulator.js
@@ -4,18 +4,31 @@ console.log("welcome to Toy Robot Simulator");
 // when robot 'reports', returns the current coords and facing
 
 var cardinalDirection = ["north", "south", "east", "west"];
-var robotDirection = "north";
+var robotFacing;
 var robotPosition;
 var tableSize = [5,5];
 
 var report = function() {
-	console.log('robot is at ' + robotPosition + ', facing ' + robotDirection + '.');
+	console.log('robot is at ' + robotPosition + ', facing ' + robotFacing + '.');
 };
 
-
-var place = function(x,y,f) {
-	robotPosition = (x,y);
-	robotDirection = f;
+var place = function(x, y, f) {
+	robotPosition = [x, y];
+	robotFacing = f;
 	console.log('robot placed on table');
 	report();
 };
+
+var placedCheck = function() {
+	if (robotPosition !== undefined) {
+		return true;
+		} else {
+			return false;
+		}
+};
+
+// var move = function() {
+// 	if (placedCheck()) {
+
+// 	}
+// }
